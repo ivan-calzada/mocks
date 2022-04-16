ec99b070454a7a7cc8944ea43a85ff143036668d
ivan <carlosivancalzadab@gmail.com>
2022-04-16 18:40:38 -0500

    creando ejemplo cliente thymeleaf

---
+++ Spring-learn/Thymeleaf/Clienthymeleaf/src/main/java/com/thymeleaf/client/ClienthymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.thymeleaf.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienthymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienthymeleafApplication.class, args);
+	}
+
+}
---
+++ Spring-learn/Thymeleaf/Clienthymeleaf/src/test/java/com/thymeleaf/client/ClienthymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thymeleaf.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienthymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
