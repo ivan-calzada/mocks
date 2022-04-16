f5e2d67d1d63ef5eb0120bec6be18664850642de
ivan <carlosivancalzadab@gmail.com>
2022-04-15 21:27:47 -0500

    iniciando ejemplo thymeleaf

---
+++ Spring-learn/Thymeleaf/ApiDisquera/src/main/java/com/api/disquera/ApiDisqueraApplication.java
@@ -0,0 +1,13 @@
+package com.api.disquera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDisqueraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDisqueraApplication.class, args);
+	}
+
+}
---
+++ Spring-learn/Thymeleaf/ApiDisquera/src/test/java/com/api/disquera/ApiDisqueraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.disquera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDisqueraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
