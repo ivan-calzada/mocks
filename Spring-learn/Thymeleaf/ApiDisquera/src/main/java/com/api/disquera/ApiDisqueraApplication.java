61c00c3650961cddd25eae1a3bef6a419339ccf3
ivan <carlosivancalzadab@gmail.com>
2022-04-16 21:51:22 -0500

    agregando variable de entorno

--- Spring-learn/Thymeleaf/ApiDisquera/src/main/java/com/api/disquera/ApiDisqueraApplication.java
+++ Spring-learn/Thymeleaf/ApiDisquera/src/main/java/com/api/disquera/ApiDisqueraApplication.java
@@ -2,7 +2,9 @@ package com.api.disquera;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ApiDisqueraApplication {
 
